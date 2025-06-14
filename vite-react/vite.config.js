c2e3394456b69d4078174bc943c9f97d3954a900
hjseo <seohj92@gmail.com>
2025-06-14 18:58:40 +0900

    workflows

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -5,16 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/vite-react/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
   css: {
     postcss: './postcss.config.js'
   }
