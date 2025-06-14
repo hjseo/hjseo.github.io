aad9a5818b9abbf1261dbbfcdf75538d807b00f7
hjseo <seohj92@gmail.com>
2025-06-14 19:10:45 +0900

    url 경로 수정정

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-react/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   css: {
     postcss: './postcss.config.js'
   }
